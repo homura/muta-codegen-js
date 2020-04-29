09ee1eac89cca48bfd9bd79f204165c1c11653b2
homura <homura.dev@gmail.com>
2020-04-29 21:37:41 +0800

    feat(cli): supported fetch schema from API

--- packages/muta-codegen-cli/src/fetch.ts
+++ packages/muta-codegen-cli/src/fetch.ts
@@ -1,15 +1,46 @@
-import request from 'request-promise';
+import fetch from 'node-fetch';
 
-export async function fetchSchema(endpoint: string): Promise<string> {
-  const res = await request(endpoint, {
+/**
+ * service schema source, it is [GraphQL](https://graphql.org/learn/schema/)
+ * like syntax
+ */
+type Source = string;
+
+export interface SchemaSource {
+  /**
+   * service name
+   */
+  service: string;
+  /**
+   * method schema
+   */
+  method: Source;
+  /**
+   * event schema source
+   */
+  event: Source;
+}
+
+export async function fetchSchemaSource(
+  endpoint: string,
+): Promise<SchemaSource[]> {
+  const res = await fetch(endpoint, {
     method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
+    headers: { 'content-type': 'application/json' },
     body: JSON.stringify({
-      query: '{getSchema}',
+      query: `
+{
+  getSchema {
+    schema {
+      service
+      method
+      event
+    }
+  }
+}`,
     }),
   });
 
-  return JSON.parse(res)?.data?.getSchema;
+  const json = await res.json();
+  return json?.data?.getSchema?.schema ?? [];
 }
